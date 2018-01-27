810d532b29bec8e2f3765704f86420ad256bf3c1
Vy Hong <contact@vyhong.me>
2018-01-27 13:21:20 -0800

    Move gitconfig to linux.gitconfig

--- symlink.sh
+++ symlink.sh
@@ -6,22 +6,7 @@ if [[ ! -d $DOTFILES_DIR ]]; then
     exit
 fi
 
-if [[ $OSTYPE == darwin* ]]; then
-    echo Detect Mac OS.
-
-    echo Linking tmux...
-    ln -sf $DOTFILES_DIR/tmux/mac.tmux.conf $HOME/.tmux.conf
-    echo Done.
-
-    BASH_MAC=$DOTFILES_DIR/bash/mac
-    echo Linking .bash_profile...
-    ln -sf $BASH_MAC/bash_profile $HOME/.bash_profile
-    echo Done.
-
-    echo Linking .bash_aliases...
-    ln -sf $BASH_MAC/bash_aliases $HOME/.bash_aliases
-    echo Done.
-elif [[ $OSTYPE == linux* ]]; then
+if [[ $OSTYPE == linux* ]]; then
     echo Detect Linux OS.
 
     echo Linking tmux...
@@ -39,5 +24,25 @@ elif [[ $OSTYPE == linux* ]]; then
 
     echo Linking .bash_aliases...
     ln -sf $BASH_LINUX/bash_aliases $HOME/.bash_aliases
+    echo Done.
+
+    GIT_DIR=$DOTFILES_DIR/git
+    echo Linking .gitconfig...
+    ln -sf $GIT_DIR/linux.gitconfig $HOME/.gitconfig
+    echo Done.
+elif [[ $OSTYPE == darwin* ]]; then
+    echo Detect Mac OS.
+
+    echo Linking tmux...
+    ln -sf $DOTFILES_DIR/tmux/mac.tmux.conf $HOME/.tmux.conf
+    echo Done.
+
+    BASH_MAC=$DOTFILES_DIR/bash/mac
+    echo Linking .bash_profile...
+    ln -sf $BASH_MAC/bash_profile $HOME/.bash_profile
+    echo Done.
+
+    echo Linking .bash_aliases...
+    ln -sf $BASH_MAC/bash_aliases $HOME/.bash_aliases
     echo Done.
 fi
