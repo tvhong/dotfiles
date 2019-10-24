aeb1240f800fcd34b1d125d2aeec112f59bfe038
Vy Hong <vhong@amazon.com>
2019-10-24 16:13:41 -0700

    Link zshrc in symlink.sh

--- symlink.sh
+++ symlink.sh
@@ -10,6 +10,42 @@ TMUX_DIR=$DOTFILES_DIR/tmux
 BASH_DIR=$DOTFILES_DIR/bash
 GIT_DIR=$DOTFILES_DIR/git
 CTAGS_DIR=$DOTFILES_DIR/ctags
+ZSH_DIR=$DOTFILES_DIR/zsh
+
+copy_with_backup() {
+    if [[ $# -ne 2 ]]; then
+        echo ERROR: Calling copy_with_backup with incorrect arguments
+    fi
+
+    src="$1"
+    dest="$2"
+    dest_bk=$(mktemp ${dest}.bk.XXXXXX)
+    if [[ ! -e "$src" ]]; then
+        echo "$src" does not exist. Skipping...
+        return 1
+    fi
+
+    if [[ -e "$dest" ]]; then
+        echo Backing up "$dest" to "$dest_bk"
+        mv "$dest" "$dest_bk"
+    fi
+
+    ln -s "$src" "$dest"
+}
+
+linking() {
+    if [[ $# -ne 2 ]]; then
+        echo ERROR: Calling linking with incorrect arguments
+    fi
+
+    src="$1"
+    dest="$2"
+    echo ---
+    echo Linking "$src" to "$dest"...
+    copy_with_backup "$src" "$dest"
+    echo Done
+}
+    
 
 if [[ $OSTYPE == linux* ]]; then
     echo OS: Linux.
@@ -48,6 +84,8 @@ elif [[ $OSTYPE == darwin* ]]; then
     echo Linking .bash_profile...
     ln -sf $BASH_MAC/bash_profile $HOME/.bash_profile
     echo Done.
+
+    linking "$ZSH_DIR/zshrc" "$HOME/.zshrc"
 
     echo Linking .gitconfig...
     ln -sf $GIT_DIR/mac.gitconfig $HOME/.gitconfig
