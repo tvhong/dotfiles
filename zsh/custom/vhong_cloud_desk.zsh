550f5b7f34ead2afcc222b0b43a787de149d8f50
Vy Hong <contact@vyhong.me>
2019-11-01 19:25:04 +0000

    Override git editor in cloud desk

--- zsh/custom/vhong_cloud_desk.zsh
+++ zsh/custom/vhong_cloud_desk.zsh
@@ -7,5 +7,6 @@ alias brazil-octane='/apollo/env/OctaneBrazilTools/bin/brazil-octane'
 alias third-party-promote='~/.toolbox/bin/brazil-third-party-tool promote'
 alias third-party='~/.toolbox/bin/brazil-third-party-tool'
 
+export GIT_EDITOR=vim
 alias vim='/apollo/env/envImprovement/bin/vim'
 alias vi='/apollo/env/envImprovement/bin/vim'
