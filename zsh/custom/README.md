1cc9fe2128739611528c036e3825f3e4ba095cdf
Vy Hong <vhong@amazon.com>
2019-11-01 10:52:27 -0700

    Add machine specific init script for zsh

---
+++ zsh/custom/README.md
@@ -0,0 +1,5 @@
+This directory contains machine specific init scripts for zsh.
+
+```
+ln -s $HOME/.dotfiles/zsh/custom/drive_knight.zsh $HOME/.oh-my-zsh/custom/drive_knight.zsh
+```
---
+++ zsh/custom/drive_knight.zsh
@@ -0,0 +1,4 @@
+# Files on the custom/ directory will be automatically loaded by the init
+# script, in alphabetical order.
+
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/amazon-corretto-8.jdk/Contents/Home
