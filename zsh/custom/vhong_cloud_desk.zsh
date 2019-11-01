0787639317bd47ba19f692557313cbf25076cf84
Vy Hong <contact@vyhong.me>
2019-11-01 19:19:16 +0000

    Alias vim and vi for cloud desktop

--- zsh/custom/vhong_cloud_desk.zsh
+++ zsh/custom/vhong_cloud_desk.zsh
@@ -6,4 +6,6 @@ export PATH="/apollo/env/AmazonAwsCli/bin/:$PATH"
 alias brazil-octane='/apollo/env/OctaneBrazilTools/bin/brazil-octane'
 alias third-party-promote='~/.toolbox/bin/brazil-third-party-tool promote'
 alias third-party='~/.toolbox/bin/brazil-third-party-tool'
+
 alias vim='/apollo/env/envImprovement/bin/vim'
+alias vi='/apollo/env/envImprovement/bin/vim'
