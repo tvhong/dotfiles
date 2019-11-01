2776db6e38fa3a9c5f82f6d5edb9ecc1cf6822a5
Vy Hong <vhong@amazon.com>
2019-11-01 11:00:07 -0700

    Move export path to drive_knight custom script

--- zsh/custom/drive_knight.zsh
+++ zsh/custom/drive_knight.zsh
@@ -1,4 +1,5 @@
 # Files on the custom/ directory will be automatically loaded by the init
 # script, in alphabetical order.
 
+export PATH=$HOME/Library/Python/3.7/bin:$HOME/.toolbox/bin:$PATH
 export JAVA_HOME=/Library/Java/JavaVirtualMachines/amazon-corretto-8.jdk/Contents/Home
