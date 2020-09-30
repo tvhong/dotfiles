91c7b3e02316255f7539e68c3d3f4022dd45fac7
Vy Hong <vhong@amazon.com>
2020-09-30 15:49:23 -0700

    Update Python library in drive_knight

--- zsh/custom/drive_knight.zsh
+++ zsh/custom/drive_knight.zsh
@@ -1,7 +1,9 @@
 # Files on the custom/ directory will be automatically loaded by the init
 # script, in alphabetical order.
 
-export PATH=$HOME/Library/Python/3.7/bin:$HOME/.toolbox/bin:$PATH
+# AWS CLI is installed here
+export PATH=$HOME/Library/Python/3.8/bin:$PATH
+export PATH=$HOME/.toolbox/bin:$PATH
 export JAVA_HOME=/Library/Java/JavaVirtualMachines/amazon-corretto-8.jdk/Contents/Home
 
 alias bb=brazil-build
